1b0a4311351498dfc6d316d9289cae4fae143618
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-11-16 15:17:23 -0300

    Primeiro commit

---
+++ src/main/java/com/gabrielsales/AEliteBarberShop/AEliteBarberShopApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsales.AEliteBarberShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AEliteBarberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AEliteBarberShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielsales/AEliteBarberShop/AEliteBarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsales.AEliteBarberShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AEliteBarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
