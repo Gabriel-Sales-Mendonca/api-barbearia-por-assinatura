f00c0f0e338c08bae00130888e697430c02cab75
Gabriel Sales <gabrielsales07112018@gmail.com>
2026-02-05 10:00:44 -0300

    feat: Adiciona a captura de uma nova exceção no ExceptionHandler

--- src/main/java/com/gabrielsales/AEliteBarberShop/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/gabrielsales/AEliteBarberShop/controllers/exceptions/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -93,6 +94,20 @@ public class ControllerExceptionHandler {
                 Instant.now(),
                 status.value(),
                 "Argument not valid",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(error);
+    }
+
+    @ExceptionHandler(CredentialsExpiredException.class)
+    public ResponseEntity<StandardError> credentialsExpired(CredentialsExpiredException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        StandardError error = new StandardError(
+                Instant.now(),
+                status.value(),
+                "Credential expired",
                 e.getMessage(),
                 request.getRequestURI()
         );
