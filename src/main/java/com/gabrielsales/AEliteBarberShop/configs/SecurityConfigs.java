1703af3f0f4b697977b0403a4a9433c47d4b9681
Gabriel Sales <gabrielsales07112018@gmail.com>
2026-02-05 11:27:36 -0300

    refactor: Adiciona uma rota como permitida nas configurações do Spring Security

--- src/main/java/com/gabrielsales/AEliteBarberShop/configs/SecurityConfigs.java
+++ src/main/java/com/gabrielsales/AEliteBarberShop/configs/SecurityConfigs.java
@@ -38,6 +38,7 @@ public class SecurityConfigs {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(autorize -> autorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/verify-email").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/plans").permitAll()
                         .requestMatchers( "/error").permitAll()
