c6996baf7b1f3e6e19c823b56f4b7ad56d688dab
Gabriel Sales <gabrielsales07112018@gmail.com>
2026-02-12 08:16:47 -0300

    test: Deixa os testes mais isolados

--- src/test/java/com/gabrielsales/AEliteBarberShop/services/OrderServiceTest.java
+++ src/test/java/com/gabrielsales/AEliteBarberShop/services/OrderServiceTest.java
@@ -6,6 +6,7 @@ import com.gabrielsales.AEliteBarberShop.entities.Plan;
 import com.gabrielsales.AEliteBarberShop.entities.User;
 import com.gabrielsales.AEliteBarberShop.repositories.OrderRepository;
 import com.gabrielsales.AEliteBarberShop.services.exceptions.ResourceAlreadyExistsException;
+import com.gabrielsales.AEliteBarberShop.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -37,9 +39,6 @@ class OrderServiceTest {
     private Plan plan;
 
     @Mock
-    private Order order;
-
-    @Mock
     private OrderRepository orderRepository;
 
     @Mock
@@ -53,16 +52,15 @@ class OrderServiceTest {
 
     @BeforeEach
     void setUp() {
-        user.setId(1L);
-        plan.setPrice(1.0);
-
         BDDMockito.given(this.userService.getTokenUser()).willReturn(user);
-        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
     }
 
     @Test
     @DisplayName("Should throw exception when already exist order with status AWAITING_PROOF_OF_PAYMENT")
     void create_ShouldThrowException_WHenAlreadyExistOrderWithStatusAWAITING_PROOF_OF_PAYMENT() {
+        user.setId(1L);
+        plan.setPrice(1.0);
+
         Order order = new Order(
                 plan.getPrice(),
                 LocalDate.now(ZoneId.of("America/Sao_Paulo")),
@@ -71,6 +69,7 @@ class OrderServiceTest {
                 plan
         );
 
+        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
         BDDMockito.given(this.orderRepository.findAllByUserIdAndOrderStatusIn(any(), any())).willReturn(order);
 
         Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> this.orderService.create(1L));
@@ -79,6 +78,9 @@ class OrderServiceTest {
     @Test
     @DisplayName("Should throw exception when already exist order with status AWAITING_PAYMENT_APPROVAL")
     void create_ShouldThrowException_WHenAlreadyExistOrderWithStatusAWAITING_PAYMENT_APPROVAL() {
+        user.setId(1L);
+        plan.setPrice(1.0);
+
         Order order = new Order(
                 plan.getPrice(),
                 LocalDate.now(ZoneId.of("America/Sao_Paulo")),
@@ -87,6 +89,7 @@ class OrderServiceTest {
                 plan
         );
 
+        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
         BDDMockito.given(this.orderRepository.findAllByUserIdAndOrderStatusIn(any(), any())).willReturn(order);
 
         Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> this.orderService.create(1L));
@@ -95,6 +98,9 @@ class OrderServiceTest {
     @Test
     @DisplayName("Doesn't should throw exception when already exist order with status PAYMENT_APPROVED")
     void create_DoesNotShouldThrowException_WHenAlreadyExistOrderWithStatusPAYMENT_APPROVED() {
+        user.setId(1L);
+        plan.setPrice(1.0);
+
         Order order = new Order(
                 plan.getPrice(),
                 LocalDate.now(ZoneId.of("America/Sao_Paulo")),
@@ -103,6 +109,7 @@ class OrderServiceTest {
                 plan
         );
 
+        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
         BDDMockito.given(this.orderRepository.findAllByUserIdAndOrderStatusIn(any(), any())).willReturn(order);
 
         Assertions.assertDoesNotThrow(() -> this.orderService.create(1L));
@@ -111,6 +118,9 @@ class OrderServiceTest {
     @Test
     @DisplayName("Doesn't should throw exception when already exist order with status PAYMENT_REJECTED")
     void create_DoesNotShouldThrowException_WHenAlreadyExistOrderWithStatusPAYMENT_REJECTED() {
+        user.setId(1L);
+        plan.setPrice(1.0);
+
         Order order = new Order(
                 plan.getPrice(),
                 LocalDate.now(ZoneId.of("America/Sao_Paulo")),
@@ -119,6 +129,7 @@ class OrderServiceTest {
                 plan
         );
 
+        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
         BDDMockito.given(this.orderRepository.findAllByUserIdAndOrderStatusIn(any(), any())).willReturn(order);
 
         Assertions.assertDoesNotThrow(() -> this.orderService.create(1L));
@@ -127,6 +138,9 @@ class OrderServiceTest {
     @Test
     @DisplayName("Doesn't should throw exception when already exist order with status CANCELED_ORDER")
     void create_DoesNotShouldThrowException_WHenAlreadyExistOrderWithStatusCANCELED_ORDER() {
+        user.setId(1L);
+        plan.setPrice(1.0);
+
         Order order = new Order(
                 plan.getPrice(),
                 LocalDate.now(ZoneId.of("America/Sao_Paulo")),
@@ -135,9 +149,18 @@ class OrderServiceTest {
                 plan
         );
 
+        BDDMockito.given(this.planService.findById(anyLong())).willReturn(plan);
         BDDMockito.given(this.orderRepository.findAllByUserIdAndOrderStatusIn(any(), any())).willReturn(order);
 
         Assertions.assertDoesNotThrow(() -> this.orderService.create(1L));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when the order doesn't exist")
+    void findById_ShouldThrowException_WhenOrderDoesNotExist() {
+        BDDMockito.given(this.orderRepository.findById(any())).willReturn(Optional.empty());
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> this.orderService.findById(1L));
     }
 
 }
